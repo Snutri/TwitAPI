74cff71c3808c0289823352f5ec1351582f9f080
AAoamk <60188000+AAoamk@users.noreply.github.com>
2022-08-29 17:46:27 +0300

    fixing up formatting and gui

--- receivers.py
+++ receivers.py
@@ -1,4 +1,5 @@
 from optparse import Values
+import threading
 import requests
 from requests_oauthlib import OAuth1Session
 import os
@@ -6,39 +7,14 @@ import json
 from savers import SendToFile, SendToTerminal, SendToArchive, SendToImages
 from dotenv import load_dotenv 
 load_dotenv()
-import time
-#consumer_key = os.getenv("APIKey")
-#consumer_secret = os.getenv("APIKeySecret")
-#bearer_token = os.getenv("BearerToken")
+from time import sleep
 
-#def UserLikeLookUp(USER_ID, TwtCount):
-#    url = f"https://api.twitter.com/2/users/{USER_ID}/liked_tweets?max_results={TwtCount}"
-#    json_response = connect_to_endpoint(url)
-#    SendToTerminal(json_response)
-#    #SendToArchive(json_response, USER_ID, "Userlikes")
-#    return json_response
-#
-#def UserTweetLookUp(USER_ID, user, TwtCount):
-#    url = f"https://api.twitter.com/2/users/{USER_ID}/tweets?max_results={TwtCount}"
-#    json_response = connect_to_endpoint(url)
-#    #SendToTerminal(json_response)
-#    SendToArchive(json_response, "Tweets", user)
-#    return json_response
-
-#def TweetLikerLookUp(TWEET_ID):
-#    url = "https://api.twitter.com/2/tweets/{}/liking_users".format(TWEET_ID)
-#    json_response = connect_to_endpoint(url)
-#    SendToTerminal(json_response)
-#    return json_response
-
-#------------------------------------------------------------------------
-def image_handler(user):
+def image_handler(user, p_call, r_call):
     #fetching images from users timeline json
     with open(f'{user}-TimeLine.json', 'r', encoding='utf-8') as j:
         tweets = json.loads(j.read())
         if not tweets:
             print("empty list of tweets")
-        x = "fill"
         i = 0
         try:
             path = f"{user}_images"
@@ -46,58 +22,67 @@ def image_handler(user):
             print("Directory " , path ,  " Created ") 
         except FileExistsError:
             print("Directory " , path ,  " already exists")
-        while x != "":
+        while True:
             try:
                 MUrl = tweets["includes"]["media"][i].get('url')
                 MKey = tweets["includes"]["media"][i].get('media_key')
                 SendToImages(path, MUrl, MKey)
-                print(f"saving image: {MUrl}")
+                r_call.emit(f"saving image: {MUrl}")
                 i += 1
+                
             except:
-                print("reached end of tweets")
+                r_call.emit("Reached end of tweets")
                 break
 
         print("all images downloaded")
 
 
-def user_timeline_fetch(USER_ID, user, TwtCount):
+def user_timeline_fetch(USER_ID, user, tweet_count, p_call, r_call):
     query = "&expansions=attachments.media_keys&tweet.fields=created_at,public_metrics&media.fields=url&exclude=retweets,replies"
-    type = "TimeLine"
-    url_formatter(USER_ID, user, TwtCount, query, type)
+    request_type = "TimeLine"
+    thread_identity= threading.get_ident()
+    r_call.emit(f"thread:[{thread_identity}] is pulling tweets from: [{user}]")
+    url_formatter(USER_ID, user, tweet_count, query, request_type, p_call, r_call)
     return
 
-def image_searcher(USER_ID, user, TwtCount):
+def image_searcher(USER_ID, user, tweet_count, p_call, r_call):
     query = "&expansions=attachments.media_keys&media.fields=url&exclude=retweets,replies"
-    type = "Images"
+    request_type = "Images"
     print("doing image tweet fetch now!")
-    url_formatter(USER_ID, user, TwtCount, query, type)
+    url_formatter(USER_ID, user, tweet_count, query, request_type, p_call, r_call)
     return
 
-def url_formatter(USER_ID, user, TwtCount, query, type):
+def url_formatter(USER_ID, user, tweet_count, query, type, p_call, r_call):
     next_token = ""
     h = 100
-    TwtCount = int(TwtCount)
-    while TwtCount > 0:
-            time.sleep(1)
-            #case where its the first search, and has more than 100 tweets
-            if (TwtCount > h) and (next_token == ""):
-                next_token = tweet_puller(USER_ID, user, h, next_token, query, type)
-                TwtCount -= h
-            #case where its the first search, and has less than 100 tweets
-            elif (TwtCount < h) and (next_token == ""):
-                next_token = tweet_puller(USER_ID, user, TwtCount, next_token, query, type)
-                print("all wanted tweets read, finishing fetch!")
-                return 1
-            #case where there is a next token, and theres more than 100 tweets remaining
-            elif int(TwtCount) > h:
-                next_token = tweet_puller(USER_ID, user, h, next_token, query, type)
-                TwtCount -= h
-            #case where there is less than 100 tweets remaining and no token, or something messed up
-            else:
-                tweet_puller (USER_ID, user, TwtCount, next_token, query, type)
-                next_token = ""
-                print("all wanted tweets read, finishing fetch!")
-                return 
+    original_count = tweet_count
+    tweet_count = int(tweet_count)
+    while tweet_count > 0:
+        #case where its the first search, and has more than 100 tweets
+        if (tweet_count > h) and (next_token == ""):
+            next_token = tweet_puller(USER_ID, user, h, next_token, query, type)
+            tweet_count -= h
+            p_call.emit(int(1/(int(original_count)/(int(original_count) - int(tweet_count)))*100))
+        #case where its the first search, and has less than 100 tweets
+        elif (tweet_count < h) and (next_token == ""):
+            next_token = tweet_puller(USER_ID, user, tweet_count, next_token, query, type)
+            tweet_count = 0
+            p_call.emit(int(1/(int(original_count)/(int(original_count) - int(tweet_count)))*100))
+            r_call.emit("all wanted tweets read, finishing fetch!\n")
+            return 1
+        #case where there is a next token, and theres more than 100 tweets remaining
+        elif int(tweet_count) > h:
+            next_token = tweet_puller(USER_ID, user, h, next_token, query, type)
+            tweet_count -= h
+            p_call.emit(int(1/(int(original_count)/(int(original_count) - int(tweet_count)))*100))
+        #case where there is less than 100 tweets remaining and no token, or something messed up
+        else:
+            tweet_puller (USER_ID, user, tweet_count, next_token, query, type)
+            next_token = ""
+            tweet_count = 0
+            p_call.emit(int(1/(int(original_count)/(int(original_count) - int(tweet_count)))*100))
+            r_call.emit("all wanted tweets read, finishing fetch!\n")
+            return 
 
 
 def tweet_puller (USER_ID, user, max_results, next_token, query, type):
@@ -108,7 +93,6 @@ def tweet_puller (USER_ID, user, max_results, next_token, query, type):
     SendToArchive(json_response, type, user,)
     try:
         next_token = json_response["meta"]["next_token"]
-        print(next_token)
     except KeyError:
         print("no next token returned")
         return
@@ -121,43 +105,35 @@ def bearer_oauth(r):
     return r
 
 def look_up_user(u):
-    # a method to look up a user by their username, returning their id
     ux = f"https://api.twitter.com/2/users/by/username/{u}"
     json_response = connect_to_endpoint(ux)
-    userID = json_response["data"]["id"]
-    #print(userID)
-    return userID
+    return json_response["data"]["id"]
 
-def connect_to_endpoint(url):
+def connect_to_endpoint(url):  # sourcery skip: raise-specific-error
     response = requests.request("GET", url, auth=bearer_oauth)
-    print(response.status_code)
     if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
-        )
+        raise Exception(f"Request returned an error: {response.status_code} {response.text}")
+
     return response.json()
 
 
-def api_script(state, tweet_count, user, secret, bearer, token):
+def api_script(state, tweet_count, user, secret, bearer, token, p_call, r_call):
     global bearer_token
     bearer_token = str(bearer)
-    #print("---------------------------------------")
-    #UserLikeLookUp(look_up_user("usernamehere"), "5")
-    #TweetLikerLookUp("useridhere")
+    print(threading.get_ident())
+    state_director(state, tweet_count, user, secret, bearer, token, p_call, r_call)
+
+def state_director(state, tweet_count, user, secret, bearer, token, p_call, r_call):
     match state:
         case 1:
-            s = user_timeline_fetch(look_up_user(user), user, f"{tweet_count}")
+            s = user_timeline_fetch(look_up_user(user), user, f"{tweet_count}", p_call, r_call)
             return s
         case 2:
-            s = image_handler(user)
+            s = image_handler(user, p_call, r_call)
             return s
         case 3:
             s = user_timeline_fetch(look_up_user(user), user, f"{tweet_count}")
             return s
-            
-
 
 if __name__ == "__main__":
     api_script()
--- savers.py
+++ savers.py
@@ -7,9 +7,9 @@ import jsonmerge
 import requests
 import sys
 import shutil
-time = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H%MZ")
+c_timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d-%H%MZ")
 def SendToFile(jsonfile, searchterm, type):
-    with open(f'{type}-{searchterm}-{time}.json', 'w', encoding='utf-8') as f:
+    with open(f'{type}-{searchterm}-{c_timestamp}.json', 'w', encoding='utf-8') as f:
         json.dump(jsonfile, f, ensure_ascii=False, indent=4)
     return 1
 
@@ -21,11 +21,7 @@ def SendToArchive(jsonfile, searchterm, user):
     filename=f'{user}-{searchterm}.json'
 
     if (os.path.exists(filename) and (os.path.getsize(filename)>0)):
-        
-        #loaded1 = json.load(open(filename))
-        #loaded2 = jsonfile
-#
-        #loaded1['data'].append(loaded2['data'])
+
         with open(filename,'r+', encoding='utf-8') as file:
         
             a = json.load(file)
@@ -69,52 +65,18 @@ def SendToArchive(jsonfile, searchterm, user):
     return 1
 
 def SendToImages(path, image, key):
-  count = 0
-  size="large"
-  if image:
-      # image's path with a new name
-      ext = os.path.splitext(image)[1]
-      
-      name = key + ext 
-      save_dest = os.path.join(path, name)
-      # save the image in the specified directory if
-      if not (os.path.exists(save_dest)):
-          r = requests.get(image + ":" + size, stream=True)
-          if r.status_code == 200:
-              with open(save_dest, "wb") as f:
-                  r.raw.decode_content = True
-                  shutil.copyfileobj(r.raw, f)
-              count += 1
-              print(f"{name} saved")
-      else:
-          print(f"Skipping {name}: already downloaded")
-#comments of shame on appending to file
-
-            #purely adds second dicts values after the existing one
-                    #c = dict(list(a.data.items()) + list(jsonfile.data.items()))
-
-            #produces new dict after the existing ones
-                    #d = Merge(a,b2)
-
-            #seemingly doesnt work, produces null after existing one
-                    #e = b2.update(a)
-
-            #seemingly doesnt work, produces one string line after the existing one
-                    #g = {**a, **b2}
-
-            #method h, also didnt work, had trouble with it thinking the values being dict
-                    #df1 = pd.read_json(a, lines=True)
-                    #df2 = pd.read_json(jsonfile, lines=True)
-                    #       
-                    #df = df1.merge(df2, on='id')
-                    #print(df)
-
-            #method i, again a failure, coulnt pinpoint data attribute
-                    #dictA = a
-                    #dictB = jsonfile
-                    #
-                    #merged_dict = {key: value for (key, value) in (dictA.data() + dictB.data())}
-                    #
-                    ## string dump of the merged dict
-                    #jsonString_merged = json.dumps(merged_dict)
-                    #print(jsonString_merged)
+    if not image:
+        return
+    ext = os.path.splitext(image)[1]
+    name = key + ext
+    save_dest = os.path.join(path, name)
+    if not os.path.exists(save_dest):
+        size = "large"
+        r = requests.get(image + ":" + size, stream=True)
+        if r.status_code == 200:
+            with open(save_dest, "wb") as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+            print(f"{name} saved")
+    else:
+        print(f"Skipping {name}: already downloaded")
